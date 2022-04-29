70f1acf5c7cc95d72b5c0ac1e1d7dd96bd82f8d5
Abraham <alezama9@calstatela.edu>
2022-04-29 01:15:04 -0700

    changed layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div id="App">
+      <Header />
       <Dashboard />
     </div>
   );
--- src/components/leftSidebar/Countries.jsx
+++ src/components/leftSidebar/Countries.jsx
@@ -3,20 +3,16 @@ import Country from "./Country";
 import * as d3 from "d3";
 
 function Countries({ data, handleClick, selected }) {
-  return (
-    <div id="countries">
-      {data.map((c) => {
-        return (
-          <Country
-            selected={selected}
-            handleClick={handleClick}
-            key={c.country}
-            country={c}
-          />
-        );
-      })}
-    </div>
-  );
+  return data.map((country) => {
+    return (
+      <Country
+        selected={selected}
+        handleClick={handleClick}
+        key={country.country}
+        country={country}
+      />
+    );
+  });
 }
 
 export default Countries;
